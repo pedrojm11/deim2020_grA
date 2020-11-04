5dabdb825b3f2bebc3fbc115a45a1e2d635f3242
alhgo <correo@laultimapregunta.com>
2020-11-04 18:38:35 +0100

    Primer Commit
    Proyecto Unity a fecha de 4 de noviembre

---
+++ ZAXXON_grA/Assets/scripts/Columna.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Columna : MonoBehaviour
+{
+
+    private Vector3 Pepe = new Vector3();
+    private Sphere Jose = new Sphere();
+
+
+    private Vector3 MyPos;
+    [SerializeField] Vector3 DestPos;
+    private Vector3 FinalPos;
+
+    Sphere Personaje = new Sphere();
+
+    float mySpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpeed = Personaje.speed;
+        print(mySpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyPos = transform.position;
+        FinalPos = MyPos + DestPos * Time.deltaTime * mySpeed;
+        transform.position = FinalPos;
+        //print(MyPos);
+    }
+}
---
+++ ZAXXON_grA/Assets/scripts/CrearColumnas.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrearColumnas : MonoBehaviour
+{
+    //Variable de tipo Object que contendrá el Prefab a instanciar
+    [SerializeField] GameObject MyColumn;
+    //Variable de tipo Transform que contendrá el objeto de referencia
+    [SerializeField] Transform RefPos;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Intancio el prefab en coordenadas 0,0,0
+        //Instantiate(MyColumn);
+
+        //Instancio el prefab en la posición del objeto de referencia
+        //Como tenemos su componente Transform, le indicamos que lo que quiero es su posición
+        Instantiate(MyColumn, RefPos.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
